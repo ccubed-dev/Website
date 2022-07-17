3bcf2fde7a84eb47b349cb402b3fa3f64f9aa240
Nausher Rao <SherRao15@gmail.com>
2022-07-16 22:22:50 -0400

    Add import aliases for all folders

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,11 @@ export default defineConfig({
     plugins: [react()],
     resolve: {
         alias: {
-            "@styles" : path.resolve(__dirname, "./src/styles/")
+            "@": path.resolve(__dirname, "src"),
+            "@assets": path.resolve(__dirname, "src/assets"),
+            "@components": path.resolve(__dirname, "src/components"),
+            "@pages": path.resolve(__dirname, "src/pages"),
+            "@styles" : path.resolve(__dirname, "src/styles/")
         }
     }
 });
