daf6335c36d9c675e767c5cd239093ab74a511d6
Benjamin Sengupta <benjaminsengupta@gmail.com>
2022-08-15 17:04:18 -0400

    Add events section

---
+++ src/pages/LandingPage/EventsSection.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./EventsSection.css";
+
+export const EventsSection = () => {
+    return (
+        <section className="events">
+            <h3 className="events__heading">Past Events</h3>
+        </section>
+    );
+};
