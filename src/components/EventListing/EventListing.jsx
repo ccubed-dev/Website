e8f5e2b63170b0c32a975e17c49b7bc704f83dc8
Benjamin Sengupta <benjaminsengupta@gmail.com>
2022-08-15 21:29:12 -0400

    Finish event styling

--- src/components/EventListing/EventListing.jsx
+++ src/components/EventListing/EventListing.jsx
@@ -1,16 +1,22 @@
 import React from "react";
 import PropTypes from "prop-types";
 import "./EventListing.css";
+import { format } from "date-fns";
 
 export const EventListing = ({title, start, end, location}) => {
     return (
         <div className="event-listing">
             <div className="event-listing__date-snippet">
-                <p className="event-listing__date-snippet-text">{start}</p>
+                <p className="event-listing__date-snippet-text">
+                    {format(start, "LLL")}
+                    <p className="event-listing__date-snippet-day">{format(start, "d")}</p>
+                </p>
             </div>
             <div className="event-listing__event-content">
                 <p className="event-listing__title">{title}</p>
-                <p className="event-listing__subtitle">{start} to {end}</p>
+                <p className="event-listing__subtitle">
+                    {format(start, "eee, LLL d")} to {format(end, "eee, LLL d")}
+                </p>
                 <p className="event-listing__location">{location}</p>
             </div>
         </div>
@@ -19,7 +25,7 @@ export const EventListing = ({title, start, end, location}) => {
 
 EventListing.propTypes = {
     title: PropTypes.string.isRequired,
-    start: PropTypes.string.isRequired,
-    end: PropTypes.string.isRequired,
+    start: PropTypes.instanceOf(Date).isRequired,
+    end: PropTypes.instanceOf(Date).isRequired,
     location: PropTypes.string.isRequired,
 };
--- src/pages/LandingPage/EventsSection.jsx
+++ src/pages/LandingPage/EventsSection.jsx
@@ -2,11 +2,40 @@ import React from "react";
 import { EventListing } from "../../components/EventListing/EventListing";
 import "./EventsSection.css";
 
+const EVENTS = [
+    {
+        title: "Hawkhacks 2022",
+        start: "2022-08-13T10:00:00.000Z",
+        end: "2022-08-15T18:00:00.000Z",
+        location: "Online",
+    },
+    {
+        title: "CCubed Conference 2022",
+        start: "2022-08-13T10:00:00.000Z",
+        end: "2022-08-15T18:00:00.000Z",
+        location: "Online",
+    },
+    {
+        title: "League of Legends Tournament",
+        start: "2022-08-13T10:00:00.000Z",
+        end: "2022-08-15T18:00:00.000Z",
+        location: "Online",
+    },
+];
+
 export const EventsSection = () => {
     return (
         <section className="events">
             <h3 className="events__heading">Past Events</h3>
-            <EventListing title="Hawkhacks 2022" start="Sat, Aug 13" end="Mon, Aug 15" location="Online" />
+            {EVENTS.map((ev, idx) =>
+                <EventListing
+                    key={idx}
+                    title={ev.title}
+                    start={new Date(ev.start)}
+                    end={new Date(ev.end)}
+                    location={ev.location}
+                />
+            )}
         </section>
     );
 };
